002e22d6e238df752cecbfa8232f0bcfd79f0452
Hack Reactor Students <jonathan.reem@gmail.com>
2014-04-21 21:04:47 -0700

    Added a UI loop based on TimeStream and Planet.render.

---
+++ ui.js
@@ -0,0 +1,22 @@
+var $ = $;
+var _ = _;
+var Planet = Planet;
+var TimeStream = TimeStream;
+
+var times = function (n, callback) {
+  for (var i = 0; i < n; i++) {
+    callback();
+  }
+};
+
+$(document).ready(function () {
+
+  times(100, function () { new Planet(); });
+
+  var time = new TimeStream();
+
+  setInterval(function () {
+    time.stepTime();
+    _.invoke(Planet.planets, "render");
+  }, 17);
+});
