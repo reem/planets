5d9b799fd6bc764696401963bf4b3a8fb5b6f389
Hack Reactor Students <accounts@hackreactor.com>
2014-04-21 21:01:38 -0700

    Implemented vector and planet objects.

---
+++ planets.js
@@ -0,0 +1,95 @@
+var TIME = TIME;
+var _ = _;
+var $ = $;
+
+var Vector = function (x, y) {
+  this.x = x;
+  this.y = y;
+};
+
+Vector.prototype.magnitude = function () {
+  return Math.sqrt(this.x * this.x + this.y * this.y);
+};
+
+Vector.prototype.scale = function (scalar) {
+  return new Vector(scalar * this.x, scalar * this.y);
+};
+
+Vector.prototype.add = function (other) {
+  return new Vector(this.x + other.x, this.y + other.y);
+};
+
+Vector.prototype.normalize = function () {
+  return this.scale(1 / this.magnitude());
+};
+
+Vector.prototype.subtract = function (other) {
+  return this.add(other.scale(-1));
+};
+
+Vector.prototype.distance = function (other) {
+  return this.substract(other).magnitude();
+};
+
+Vector.prototype.isEqual = function (other) {
+  return this.x === other.x && this.y === other.y;
+};
+
+var GravitationalConstant = 1; // 6.674e-11;
+
+var gravity = function Gravity(planet1, planet2) {
+  var gravitation =
+    (GravitationalConstant * planet1.mass * planet2.mass) /
+    Math.pow(planet1.position.distance(planet2.position), 2);
+  return planet2.position.subtract(planet1.position)
+    .normalize()
+    .scale(gravitation);
+};
+
+var Planet = function (mass, position, velocity, radius, color) { // Fill in args later.
+  // Display properties
+  this.$node = $("<span class='planet'></span>");
+  this.radius = radius || 20;
+  this.color = color || "orange";
+  this.$node.css({
+    "border-radius": this.radius,
+    "color": this.color,
+  });
+
+  // Internal math stuff
+  this.mass = mass === undefined ? 100 : mass; // 1000000000 : mass;
+  this.position =
+    position || new Vector(Math.random() * 100,
+                           Math.random() * 100);
+  this.velocity =
+    velocity || new Vector(Math.random() * 1,
+                           Math.random() * 1);
+  Planet.planets.push(this);
+};
+
+Planet.planets = [];
+
+Planet.prototype.updateVelocity = function () {
+  var gravityVectors = _.map(Planet.planets,
+    _.partial(gravity, this));
+  this.velocity = _.reduce(gravityVectors,
+    function (memo, gravVector) {
+      return memo.add(gravVector.scale(TIME/this.mass));
+    },
+  this.velocity, this);
+};
+
+Planet.prototype.updatePosition = function () {
+  this.position = this.position.add(this.velocity.scale(TIME));
+};
+
+var ScaleFactor = 10;
+
+Planet.prototype.render = function () {
+  this.$node.css({
+    "border-radius": this.radius,
+    "color": this.color,
+    "top": (this.position.y * ScaleFactor) - this.radius,
+    "width": (this.position.x * ScaleFactor) - this.radius
+  });
+};
